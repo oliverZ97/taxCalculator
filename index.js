531f97a7e4a548d926d3c7dc7b7f182ffa991787
OliverZ97 <oliver@mordoor.de>
2020-06-28 11:32:27 +0200

    add brutto calc

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 //**************************************************************************************/
 //**GET VALUES OF THE INPUT FIELDS******************************************************/
 function getInputFromNetto(id) {
-    let value = document.getElementById(id).value;
-    console.log(value)
+    return document.getElementById(id).value;
 }
 
 function getInputFromTax(id) {
-    let value = document.getElementById(id).value;
-    console.log(value)
+    return document.getElementById(id).value;
 }
 //**************************************************************************************/
-
+//**CALCULATE BRUTTO TAX****************************************************************/
+function calculateBrutto(netto_id, tax_id) {
+    let netto = getInputFromNetto(netto_id);
+    let tax = getInputFromTax(tax_id);
+    let brutto = parseFloat(netto);
+    if(tax != "0") {
+        brutto = parseFloat(netto) + parseFloat(((netto/100)*tax).toFixed(2))
+    }
+    console.log("brutto", brutto);
+}
 function createInputFormula(number) {
     let content = document.getElementById("content");
     let numberOfGroups = number
-    for(let i = 0; i < numberOfGroups; i++){
+    for (let i = 0; i < numberOfGroups; i++) {
         let container = document.createElement("div");
         let nettoInput = document.createElement("input");
         let taxDropdown = document.createElement("select");
@@ -29,27 +36,28 @@ function createInputFormula(number) {
         container.setAttribute("id", "container_" + i);
 
         //add nettoInput attributes
-        nettoInput.setAttribute("id" ,"nettoInput_" + i);
+        nettoInput.setAttribute("id", "nettoInput_" + i);
         nettoInput.setAttribute("placeholder", "Netto");
         nettoInput.setAttribute("type", "text");
-        nettoInput.onchange = "getInputFromNetto(\"nettoInput_\" + i)";
+        nettoInput.onchange = () => { calculateBrutto("nettoInput_" + i, "taxType" + i) };
 
         //add taxDropdown attributes
         taxDropdown.setAttribute("id", "taxType" + i);
         taxDropdown.setAttribute("name", "taxType");
-        taxDropdown.onchange = "getInputFromTax(\"taxType\" + i)";
+        taxDropdown.onchange = () => { calculateBrutto("nettoInput_" + i, "taxType" + i) };
+
 
         //add bruttoInput attributes
-        bruttoInput.setAttribute("id", "bruttoInput_"+ i);
+        bruttoInput.setAttribute("id", "bruttoInput_" + i);
         bruttoInput.setAttribute("placeholder", "Brutto");
         bruttoInput.setAttribute("type", "text");
 
         //add option attributes
-        taxZero.setAttribute("value", "zero");
+        taxZero.setAttribute("value", "0");
         taxZero.text = "0%";
-        taxSeven.setAttribute("value", "seven");
+        taxSeven.setAttribute("value", "7");
         taxSeven.text = "7%";
-        taxNineteen.setAttribute("value", "nineteen");
+        taxNineteen.setAttribute("value", "19");
         taxNineteen.text = "19%"
 
         //append options to dropdown
